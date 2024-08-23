cc11ad4c21a2c9beaf5169e97c0c0ed8c74e002a
andoniskgr <andoniskgr@gmail.com>
2024-08-23 22:21:17 +0300

    func

--- functions.py
+++ functions.py
@@ -1,16 +1,16 @@
-# from sms_sender import SMSSender  # Import the SMSSender class
+from sms_sender import SMSSender  # Import the SMSSender class
 
 
-# def send_sms(sms_msg="You send an empty message!"):
-#     """
-#     Purpose: send sms messages
-#     """
+def send_sms(sms_msg="You send an empty message!"):
+    """
+    Purpose: send sms messages
+    """
     
-#     # Initialize SMSSender
-#     sms_sender = SMSSender()    
+    # Initialize SMSSender
+    sms_sender = SMSSender()    
     
-#     # send sms
-#     sms_sender.send_sms(sms_msg)    
+    # send sms
+    sms_sender.send_sms(sms_msg)    
     
-# # end def
+# end def
 
